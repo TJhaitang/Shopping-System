004a6ef78e4ab2b29157a6d0c283cd0d94450f36
fbkkkk <2260805804@qq.com>
2021-11-06 01:56:16 +0800

    。

--- src/components/Message/Message.vue
+++ src/components/Message/Message.vue
@@ -1,7 +1,23 @@
 <template>
     <div>
-        <h1>消息</h1>
+        <ul>
+            <router-link to="/home">
+                <i></i>
+                <span>首页</span>
+            </router-link>
+            <router-link to="/message">
+                <i></i>
+                <span>消息</span>
+            </router-link>
+            <router-link to="/my">
+                <i></i>
+                <span>我的</span>
+            </router-link>
+        </ul>
+       <h1>消息</h1> 
+       
     </div>
+    
 </template>
 
 <script>
