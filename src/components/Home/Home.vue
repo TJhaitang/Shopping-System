96634cc89ad950b3e95c153e10bec4390f42bb2e
fbkkkk <2260805804@qq.com>
2021-11-05 16:35:30 +0800

    ...

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,6 +1,19 @@
 <template>
     <div>
-        <h1>首页</h1>
+        <ul>
+            <router-link to="/home">
+                <i></i>
+                <span>首页</span>
+            </router-link>
+            <router-link to="/message">
+                <i></i>
+                <span>消息</span>
+            </router-link>
+            <router-link to="/my">
+                <i></i>
+                <span>我的</span>
+            </router-link>
+        </ul>
     </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import login from '../components/login.vue'
 import Home from '../components/Home/Home'
+import Message from '../components/Message/Message'
+import My from '../components/My/My'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/login', component: login },
   { path: '/', redirect: '/login' },
-  { path: '/home',component: Home}
+  { path: '/home',component: Home},
+  { path: '/message',component: Message},
+  { path: '/my',component: My}
 ]
 
 const router = new VueRouter({
