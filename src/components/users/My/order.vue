9caf11d5700606d38b3f46bb476effbc6031fd47
Kikier-77 <2019202104@ruc.eud.cn>
2021-12-18 21:36:49 +0800

    gai

--- src/components/users/My/order.vue
+++ src/components/users/My/order.vue
@@ -18,17 +18,22 @@
         <ul>
           <!-- 我的订单表头 -->
           <li class="order-info">
-            <div class="order-id">订单编号: {{item.order_id}}</div>
-            <div class="order-time">订单时间: {{item.order_time}}</div>
+            <div class="order-id">订单编号: {{item.code}}</div>
+            <div class="order-time">订单时间: {{item.time}}</div>
+            <div class='pro-back'>订单状态 : {{showStatus(item.status)}}
+              <el-dropdown trigger="click">
+                <i class="el-icon-setting"></i>
+                <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item  @click.native="quit">退货</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </div>
           </li>
           <li class="header">
             <div class="pro-img"></div>
-            <div class="pro-name">商品名称</div>
-            <div class="pro-price">单价</div>
+            <div class="pro-size">商品规格</div>
             <div class="pro-num">数量</div>
-            <div class="pro-total">小计</div>
             <div class="pro-comment">评价</div>
-            <div class='pro-back'>订单状态</div>
           </li>
           <!-- 我的订单表头END -->
           
@@ -72,24 +77,16 @@
     <!-- 评价对话框结束 -->
 
           <!-- 订单列表 -->
-          <li class="product-list" v-for="(commodity,i) in item.product" :key="i">
+          <li v-for="(commodity,i) in item.items" :key="i" >
+            <div class="product-list" v-if="commodity.quantity>=0">
             <div class="pro-img">
-                <img :src="commodity.product_picture" />
+                <img :src="commodity.picture" />
             </div>
-            <div class="pro-name">{{commodity.product_name}}</div>
-            <div class="pro-price">{{commodity.product_price}}元</div>
-            <div class="pro-num">{{commodity.product_num}}</div>
-            <div class="pro-total pro-total-in">{{commodity.product_price*commodity.product_num}}元</div>
+            <div class="pro-size">{{commodity.commodityId}}</div>
+            <div class="pro-num">{{commodity.quantity}}</div>
             <div class="pro-comment">
                 <el-button type="mini" icon='el-icon-edit' @click = "showCommentDialog(commodity)"></el-button>
             </div>
-            <div class="pro-back">{{showStatus(item.status)}}
-              <el-dropdown trigger="click">
-                <i class="el-icon-setting"></i>
-                <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item  @click.native="quit">退货</el-dropdown-item>
-                </el-dropdown-menu>
-              </el-dropdown>
             </div>
           </li>
         </ul>
@@ -97,7 +94,7 @@
           <div class="order-bar-left">
             <span class="order-total">
               共
-              <span class="order-total-num">{{item.quantity}}</span> 件商品
+              <span class="order-total-num">{{item.items.itemNum}}</span> 件商品
             </span>
           </div>
           <div class="order-bar-right">
@@ -128,39 +125,39 @@ export default {
   data() {
     return {
       orders: [
-         {
-         order_id:"123",//订单编号
-         order_time:'2021-12-11',//订单时间
-         product:[
-           {product_name: "哇咔咔", // 订单商品名称
-         product_picture: "http://assets.myntassets.com/v1/images/style/properties/7a5b82d1372a7a5c6de67ae7a314fd91_images.jpg", // 订单商品图片
-         product_price: "144", // 订单商品价格
-         product_num: "1", // 订单商品数量
-           },
-         ],
-         quantity:2,
-         cost:288,
-         status:'2',
-       },
-       {
-         order_id:"123",//订单编号
-         order_time:'2021-12-11',//订单时间
-         product:[
-           {product_name: "哇咔咔", // 订单商品名称
-         product_picture: "http://assets.myntassets.com/v1/images/style/properties/7a5b82d1372a7a5c6de67ae7a314fd91_images.jpg", // 订单商品图片
-         product_price: "144", // 订单商品价格
-         product_num: "1", // 订单商品数量
-           },
-           {product_name: "哇咔咔", // 订单商品名称
-         product_picture: "http://assets.myntassets.com/v1/images/style/properties/7a5b82d1372a7a5c6de67ae7a314fd91_images.jpg", // 订单商品图片
-         product_price: "144", // 订单商品价格
-         product_num: "1", // 订单商品数量
-           },
-         ],
-        quantity:2,
-        cost:288,
-        status:'3',
-      }
+      //    {
+      //    order_id:"123",//订单编号
+      //    order_time:'2021-12-11',//订单时间
+      //    product:[
+      //      {product_name: "哇咔咔", // 订单商品名称
+      //    product_picture: "http://assets.myntassets.com/v1/images/style/properties/7a5b82d1372a7a5c6de67ae7a314fd91_images.jpg", // 订单商品图片
+      //    product_price: "144", // 订单商品价格
+      //    product_num: "1", // 订单商品数量
+      //      },
+      //    ],
+      //    quantity:2,
+      //    cost:288,
+      //    status:'2',
+      //  },
+      //  {
+      //    order_id:"123",//订单编号
+      //    order_time:'2021-12-11',//订单时间
+      //    product:[
+      //      {product_name: "哇咔咔", // 订单商品名称
+      //    product_picture: "http://assets.myntassets.com/v1/images/style/properties/7a5b82d1372a7a5c6de67ae7a314fd91_images.jpg", // 订单商品图片
+      //    product_price: "144", // 订单商品价格
+      //    product_num: "1", // 订单商品数量
+      //      },
+      //      {product_name: "哇咔咔", // 订单商品名称
+      //    product_picture: "http://assets.myntassets.com/v1/images/style/properties/7a5b82d1372a7a5c6de67ae7a314fd91_images.jpg", // 订单商品图片
+      //    product_price: "144", // 订单商品价格
+      //    product_num: "1", // 订单商品数量
+      //      },
+      //    ],
+      //   quantity:2,
+      //   cost:288,
+      //   status:'3',
+      // }
       ], // 订单列表
       commentDialogVisible: false,
 
@@ -174,35 +171,36 @@ export default {
   },
   activated() {
     // 获取订单数据
-    // this.$http
-    //       .get("/member/Shopping/getOrderList.php")
-    //       .then(res => {
-    //         if (res.data.status != "fail") {
-    //           // 不为'fail'为成功, 更新vuex购物车状态
-    //           this.orders=res.data;
-    //         } else {
-    //           // 提示失败信息
-    //           this.$message.error('没获取到订单信息 呜呜');
-    //         }
-    //       });
+    this.$http
+          .get("/member/Shopping/getOrderList.php")
+           .then(res => {
+             if (res.data.status != "fail") {
+             // 不为'fail'为成功, 更新vuex购物车状态
+               this.orders=[res.data];
+               console.log(this.orders)
+             } else {
+               // 提示失败信息
+               this.$message.error('没获取到订单信息 呜呜');
+             }
+           });
   },
   watch: {
     // 通过订单信息，计算出每个订单的商品数量及总价
-    orders: function(val) {
-      let total = [];
-      for (let i = 0; i < val.length; i++) {
-        const element = val[i];
-        let totalNum = 0;
-        let totalPrice = 0;
-        for (let j = 0; j < element.length; j++) {
-          const temp = element[j];
-          totalNum += temp.product_num;
-          totalPrice += temp.product_price * temp.product_num;
-        }
-        total.push({ totalNum, totalPrice });
-      }
-      this.total = total;
-    }
+    // orders: function(val) {
+    //   let total = [];
+    //   for (let i = 0; i < val.length; i++) {
+    //     const element = val[i];
+    //     let totalNum = 0;
+    //     let totalPrice = 0;
+    //     for (let j = 0; j < element.length; j++) {
+    //       const temp = element[j];
+    //       totalNum += temp.product_num;
+    //       totalPrice += temp.product_price * temp.product_num;
+    //     }
+    //     total.push({ totalNum, totalPrice });
+    //   }
+    //   this.total = total;
+    // }
   }, 
   methods: {
     showCommentDialog(row) {
@@ -319,44 +317,32 @@ export default {
   height: 80px;
   width: 80px;
 }
-.order .content ul .pro-name {
+.order .content ul .pro-size {
   float: left;
   width: 280px;
-  padding-left: 15px;
+  padding-left: 70px;
 }
-.order .content ul .pro-name a {
+.order .content ul .pro-size a {
   color: #424242;
 }
-.order .content ul .pro-name a:hover {
+.order .content ul .pro-size a:hover {
   color: #ff6700;
-}
-.order .content ul .pro-price {
-  float: left;
-  width: 160px;
-  padding-right: 18px;
-  text-align: center;
 }
 .order .content ul .pro-num {
   float: left;
   width: 190px;
   text-align: center;
 }
-.order .content ul .pro-total {
-  float: left;
-  width: 160px;
-  padding-right: 81px;
-  text-align: right;
-}
 .order .content ul .pro-total-in {
   color: #ff6700;
 }
 .order .content ul .pro-comment {
   float: left;
-  padding-left: 0px;
+  padding-left: 300px;
 }
 .order .content ul .pro-back {
   float: left;
-  padding-left: 20px;
+  padding-left: 200px;
 }
 .order .order-bar {
   width: 1185px;
